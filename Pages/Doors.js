48617bd38706d11b1890fd1173466a0923b66f42
Serena Deng <123137017+srenadeng@users.noreply.github.com>
2025-02-01 18:14:25 -0500

    random grid function

--- Pages/Doors.js
+++ Pages/Doors.js
@@ -11,3 +11,27 @@ function search() {
      		else li[i].style.display = "none";
  	   }
 	}
+
+function randomize() {
+	var imgArr = document.getElementsByTagName("img");
+	var liArr =  document.getElementsByClassName("doorItem");
+	for (i = 0; i < imgArr.length; i++) {
+		var randWidth = Math.floor((Math.random() + 0.2)* 200);
+		imgArr[i].style.width = randWidth + "px";
+		imgArr[i].style.height = "auto";
+		randWidth = Math.floor((Math.random()+0.1)* 60);
+		liArr[i].style.padding = randWidth + "px";
+	}
+	var text = document.getElementsByTagName("p");
+	var text1 = document.getElementsByTagName("h1");
+	var text2 = document.getElementsByTagName("a");
+	for (i = 0; i < text.length; i++) {
+		text[i].style.color = "#FAFAFA";
+	} for (i = 0; i < text1.length; i++) {
+		text1[i].style.color = "#FAFAFA";
+	} for (i = 0; i < text2.length; i++) {
+		text2[i].style.color = "#FAFAFA";
+	}
+	document.body.style.background = "#0d0d0d";
+	return;
+}
